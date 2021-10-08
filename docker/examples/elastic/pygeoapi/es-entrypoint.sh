d60ae385f09175f127a418aaed267425e2dc82ac
doublebyte1 <info@doublebyte.net>
2021-10-08 12:04:34 +0200

    - installed curl on pygeoapi container

--- docker/examples/elastic/pygeoapi/es-entrypoint.sh
+++ docker/examples/elastic/pygeoapi/es-entrypoint.sh
@@ -32,7 +32,12 @@
 
 set +e
 
-echo  "Waiting for ElasticSearch container..."
+echo  "Install Curl"
+
+apt-get update -y &&
+apt-get install curl -y &&
+
+echo  "Waiting for ElasticSearch container..." &&
 
 # First wait for ES to be up and then execute the original pygeoapi entrypoint.
 /wait-for-elasticsearch.sh http://elastic_search:9200 /entrypoint.sh || echo "ES failed: $?, exit" && exit 1
