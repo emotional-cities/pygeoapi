c70d832a0b20a4fe6577e421ca29cd3163298618
doublebyte1 <info@doublebyte.net>
2021-12-10 12:14:45 +0100

    - Added Lisbon measurements in features and records

--- docker/examples/elastic/ES/add_data.sh
+++ docker/examples/elastic/ES/add_data.sh
@@ -42,11 +42,11 @@ until $(curl -sSf -XGET --insecure 'http://localhost:9200/_cluster/health?wait_f
     printf 'No status yellow from ES, trying again in 10 seconds \n'
     sleep 10
 done
-echo "Elasticsearch seems to be working - Adding ne_110m_populated_places_simple.geojson to ES"
+echo "Elasticsearch seems to be working - Adding data to ES"
 
-python3 /load_es_data.py /usr/share/elasticsearch/data/ne_110m_populated_places_simple.geojson geonameid
-python3 /load_es_data.py /usr/share/elasticsearch/data/cases2.geojson fid
-
+#python3 /load_es_data.py /usr/share/elasticsearch/data/ne_110m_populated_places_simple.geojson geonameid
+#python3 /load_es_data.py /usr/share/elasticsearch/data/cases2.geojson fid
+python3 /load_es_data.py /usr/share/elasticsearch/data/obs.geojson fid
 
 echo "Seems that data was loaded"
 
